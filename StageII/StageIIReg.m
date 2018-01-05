5087d4a4f5abb11b71e0d8288adadeb28eda64f8
Muzammil <muzammil360@gmail.com>
2018-01-05 12:33:43 -0500

    Bug Fix: StageIIReg - X,Y were being changed inplace and faulty ones were being used in interp2. code update with correct output

--- StageII/StageIIReg.m
+++ StageII/StageIIReg.m
@@ -1,11 +1,14 @@
-function OutputImg = StageIIReg(InputImg,H)
+function OutputImg = StageIIReg(InputImg,H,PrincipalPoint)
 
     % GET ALL PIXEL LOCATIONS
     ImgWidth = size(InputImg,2);
     ImgHeight = size(InputImg,1);
     
     [X,Y]=meshgrid(1:ImgWidth,1:ImgHeight);
-    P = [X(:) Y(:) ones(ImgWidth*ImgHeight,1)]';
+    Xc = X - PrincipalPoint(1);          % Shift origin to principal point 
+    Yc = Y - PrincipalPoint(2);
+    
+    P = [Xc(:) Yc(:) ones(ImgWidth*ImgHeight,1)]';
     
     % TRANSFORM ALL PIXEL LOCATIONS
 	Pt = H\P;               % equivalent to inv(H)*P
@@ -18,6 +21,9 @@ function OutputImg = StageIIReg(InputImg,H)
     XMap =reshape(xVec,ImgHeight,ImgWidth);
     YMap =reshape(yVec,ImgHeight,ImgWidth);
     
+    XMap = XMap + PrincipalPoint(1);    % Shift origin to back to top left
+    YMap = YMap + PrincipalPoint(2);
+        
     OutputImg = interp2(X,Y,InputImg,XMap,YMap);
     
 end
