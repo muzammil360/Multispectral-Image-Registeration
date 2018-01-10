0657656e93ee902d64cd77c6a9f69e5f8d79924c
Bashir <mbashir@wpi.edu>
2018-01-10 15:47:15 -0500

    StatisticComputation.m added

---
+++ Analysis/StatisticComputation.m
@@ -0,0 +1,18 @@
+% This script takes a stage 2 MRR analysis report and computes all the necessary statistics to be reported for that MRR analysis.
+clear;clc
+
+%% USER CONTROL
+InputDir = '';
+
+
+%% INITIALIZATION
+
+%% ALGORITHM
+
+% Read kth report and compute MMRR
+
+% Compute statistics
+
+%% OUTPUT
+
+% Write output csv file
