9f891c67a4e1727688a896d21269dc595eaa261f
Muzammil <muzammil360@gmail.com>
2018-01-11 14:25:02 -0500

    StatisticComputation.m code complete

--- Analysis/StatisticComputation.m
+++ Analysis/StatisticComputation.m
@@ -2,17 +2,26 @@
 clear;clc
 
 %% USER CONTROL
-InputDir = '';
+InputFileAddr = 'D:\ImageRegisterationPaper\Datasets\RedStickGolfCourse_15122016\OutputS22\MeanMRR_ElaspedTime_1.csv';
 
 
 %% INITIALIZATION
 
 %% ALGORITHM
+data = csvread(InputFileAddr);
+MMRR = data(:,1);
+Time = data(:,2);
 
-% Read kth report and compute MMRR
+MMRR(MMRR==1600) = [];
 
-% Compute statistics
+
+
 
 %% OUTPUT
 
-% Write output csv file
+Mean = mean(MMRR)
+Median = median(MMRR)
+Mode = mode(MMRR)
+Std = std(MMRR)
+Min = min(MMRR)
+Max = max(MMRR)
