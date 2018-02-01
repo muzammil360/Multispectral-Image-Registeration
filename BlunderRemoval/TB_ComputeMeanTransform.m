c13f4c36760f68a8cef9ef86c191a666201eae0d
Muzammil <muzammil360@gmail.com>
2018-02-01 11:16:23 -0500

    TB_ComputeMeanTransform2.m added

--- BlunderRemoval/TB_ComputeMeanTransform.m
+++ BlunderRemoval/TB_ComputeMeanTransform.m
@@ -3,7 +3,7 @@ clear;clc
 
 
 %% USER CONTROL
-InputDir = 'D:\ImageRegisterationPaper\Datasets\Second_test_of_GolfCourse\OutputS23_NCC';
+InputDir = 'D:\ImageRegisterationPaper\Datasets\RedStickGolfCourse_15122016\OutputS22_NCC-Com';
 MetricID = 2;
 MinInThres = 0.7;
 OutputFileName = ['MeanET' num2str(MetricID) '.csv'];
---
+++ BlunderRemoval/TB_ComputeMeanTransform2.m
@@ -0,0 +1,22 @@
+% This script computes mean transform a bunch of transforms
+clear;clc
+
+
+%% USER CONTROL
+InputDir = 'D:\ImageRegisterationPaper\Datasets\RedStickGolfCourse_15122016\OutputS22_NCC-Com';
+MetricID = 2;
+MinInThres = 0.7;
+OutputFileName = ['MeanET' num2str(MetricID) '.csv'];
+
+%% INITIALIZATION
+
+
+%% ALGORITHM
+
+%% VISUALIZATION
+
+
+
+
+%% Words
+1. 
